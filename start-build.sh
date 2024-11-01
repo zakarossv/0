bf666aebd672ceb216df688f2f18bc7ef47af507
zakarossv <zakarossvendor@gmail.com>
2024-11-02 03:09:12 +0800

    Update start-build.sh

--- start-build.sh
+++ start-build.sh
@@ -7,7 +7,7 @@ apt install tmux -y
 tmux new -s build -d
 
 # Execute commands in the new tmux session
-tmux send-keys -t build "cd /root && mkdir -p lineage && cd lineage && yes | repo init -u https://github.com/LineageOS/android.git -b lineage-17.1 && repo sync -j28" C-m
+tmux send-keys -t build "cd /root && mkdir -p lineage && cd lineage && yes | repo init -u --depth=1 https://github.com/LineageOS/android.git -b lineage-17.1 && repo sync -j28" C-m
 tmux send-keys -t build "cd /root/lineage && git clone https://github.com/sudozetaxbyte/device_realme_r5xQ.git device/realme/r5xQ" C-m
 tmux send-keys -t build "cd /root/lineage && git clone https://github.com/sudozetaxbyte/vendor_realme_r5xQ.git vendor/realme/r5xQ" C-m
 tmux send-keys -t build "cd /root/lineage && git clone https://github.com/sudozetaxbyte/kernel_realme_r5x.git kernel/realme/r5x" C-m
