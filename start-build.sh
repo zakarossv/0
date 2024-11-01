67d3a032e0735ed8e154938d434c911413173417
zakarossv <zakarossvendor@gmail.com>
2024-11-01 19:45:54 +0800

    Delete start-build.sh

--- start-build.sh
+++
@@ -1,2 +0,0 @@
-mkdir lineage && cd lineage && yes | repo init -u https://github.com/LineageOS/android.git -b lineage-17.1 && repo sync -j28 &&
-git clone https://github.com/sudozetaxbyte/device_realme_r5xQ.git device/realme/r5xQ && git clone https://github.com/sudozetaxbyte/vendor_realme_r5xQ.git vendor/realme/r5xQ && git clone https://github.com/sudozetaxbyte/kernel_realme_r5x.git kernel/realme/r5x
